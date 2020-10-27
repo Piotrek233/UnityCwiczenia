90ce8e752eb42cfcfd9e8858f4849ac95dd83ba0
Piotrek233 <32534983+Piotrek233@users.noreply.github.com>
2020-10-27 19:17:54 +0100

    zadanie1
    zadanie 1 z lab 03

--- lab03/Assets/Scripts/RuchWBok.cs
+++ lab03/Assets/Scripts/RuchWBok.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RuchWBok : MonoBehaviour
-{        // metoda Start() wywoływana jest przy inicjalizacji obiektu
+{
+    public float speed;
 
-        public float force = 1.0f;
+    private void Start()
+    {
+        speed = 0.5f;
+    }
+    void Update()
+    {
+        float pozycjaPozioma = transform.position.x + (Input.GetAxis("Horizontal") * speed);
+        float pozycjaPionowa = transform.position.z + (Input.GetAxis("Vertical") * speed);
 
-        void Start()
-        {
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.AddForce(0, 0, force, ForceMode.Impulse);
-        }
+        Vector3 nowaPozycja = new Vector3(Mathf.Clamp(pozycjaPozioma, 0f, 8f), transform.position.y, Mathf.Clamp(pozycjaPionowa, -9f, 0f));
+        transform.position = nowaPozycja;
+    }
 }
+
+
