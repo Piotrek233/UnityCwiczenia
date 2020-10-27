d27f00b609a5bd66e3f9116f24b6b622aee7496b
Piotrek233 <32534983+Piotrek233@users.noreply.github.com>
2020-10-27 15:08:16 +0100

    część zadania 1
    dodanie ruchu elementu w jedno strone

---
+++ lab03/Assets/Scripts/RuchWBok.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuchWBok : MonoBehaviour
+{        // metoda Start() wywoływana jest przy inicjalizacji obiektu
+
+        public float force = 1.0f;
+
+        void Start()
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.AddForce(0, 0, force, ForceMode.Impulse);
+        }
+}
